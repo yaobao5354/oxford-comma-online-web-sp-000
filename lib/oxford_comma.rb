a58f3445bef6164ce8ec2f2e5c972dcde90edef7
yaoyu bao <yaobao5354@gmail.com>
2019-08-18 23:02:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1 
     return array[0]
   elsif array.length == 2 
-    array[-1] = "and #{array[-1]}"
+    array.join(" and ")
   else 
     array[-1] = "and #{array[-1]}"
     array.join(", ")
